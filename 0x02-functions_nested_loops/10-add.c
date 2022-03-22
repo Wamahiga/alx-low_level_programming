e3f98af9c618371ac5902b3e620f2aa2a4d7fc8b
wamahiga <wanjirucynthia01>
2022-03-22 14:55:00 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+/*
+ * File: 10-add.c
+ * Auth: sabath
+ */
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: Result of addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+© 2022 GitHub, Inc.
+Terms
+Privacy
+S

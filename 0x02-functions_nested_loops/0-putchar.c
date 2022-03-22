c231ca45cbd9a7aa36e65a358275232f2f62730f
wamahiga <wanjirucynthia01>
2022-03-22 08:29:51 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
 
 /**
  * main - Function main entry
+ *
  * Program that prints _putchar, followed by a new line
+ *
  * Return: always 0 (success)
  */
 
